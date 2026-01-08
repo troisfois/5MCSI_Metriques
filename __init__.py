7c049826bd19f701faef6c71c5e0350fb93fb703
troisfois <salmi.alae@gmail.com>
2026-01-08 11:56:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>contact</h2>"                                                                                                                                       
+    return render_template('contact.html')                                                                                                                                     
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm3
