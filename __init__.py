fa9cb2dd708502169313392c67714aa4439596c6
troisfois <salmi.alae@gmail.com>
2026-01-08 11:13:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,6 +12,16 @@ def MaPremiereAPI():
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm3
-
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 if __name__ == "__main__":
   app.run(debug=True)
