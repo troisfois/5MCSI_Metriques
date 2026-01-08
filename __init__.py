a48cab620070ca860caa491e7c9d95ee0225cc25
troisfois <salmi.alae@gmail.com>
2026-01-08 11:18:41 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
-  @app.route("/rapport/")
+@app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
 if __name__ == "__main__":
