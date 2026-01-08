9d391d0be2dc1c8d36451563616f3058c5d0a3a8
troisfois <salmi.alae@gmail.com>
2026-01-08 11:07:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,9 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"                                                                                                                                       
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #comm3
